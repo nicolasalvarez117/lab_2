86651fdf0d0d18e6d610b7a91903daf9126ef0cd
nicolasalvarez117 <47337963+nicolasalvarez117@users.noreply.github.com>
2019-08-28 19:35:20 -0300

    Add files via upload

---
+++ ejercicio numero 16/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numero;
+            string numero2;
+            byte nota1;
+            byte nota2;
+            Alumno nuevoAlumno = new Alumno("nicolas","alvarez",1);
+            Console.WriteLine("Ingrese las notas del alumno: ");
+            Console.Write("Nota 1: ");
+
+            numero = Console.ReadLine();
+            Console.Write("Nota 2: ");
+            numero2 = Console.ReadLine();
+
+            byte.TryParse(numero, out nota1);
+            byte.TryParse(numero2, out nota2);
+
+            nuevoAlumno.Estudiar(nota1, nota2);
+            nuevoAlumno.CalcularFinal();
+
+            Console.WriteLine(nuevoAlumno.Mostrar());
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ ejercicio numero 16/alumno.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Alumno
+    {
+
+        private string nombre;
+        private string apellido;
+        private int legajo;
+        byte nota1;
+        byte nota2;
+        float notaFinal = -1;
+
+        public Alumno(string nombre,string apellido,int legajo)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.legajo = legajo;
+        }
+
+
+        public void CalcularFinal()
+        {
+            if(nota1 >= 4 && nota2 >= 4)
+            {
+                Random a = new Random();
+                notaFinal = a.Next(nota1, nota2);
+            }
+        }
+        public void Estudiar(byte notaUno,byte notaDos)
+        {
+            nota1 = notaUno;
+            nota2 = notaDos;
+        }
+        public string Mostrar()
+        {
+            string mostrar;
+            if(notaFinal != -1)
+            {
+                mostrar = nombre + " " + apellido + " " + nota1 + " " + nota2 + " " + notaFinal;
+            }
+            else
+            {
+                mostrar = nombre + " " + apellido + " "+ legajo +" " + nota1 + " " + nota2 + " " + "alumno desaprobado";
+            }
+            return mostrar;
+        }
+    }
+}
