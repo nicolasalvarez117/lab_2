664dfcbad2f5ebaee7bd4a6d43bb1544776b815a
nicolasalvarez117 <47337963+nicolasalvarez117@users.noreply.github.com>
2019-08-28 22:18:23 -0300

    Add files via upload

---
+++ ejercicio numero 17/Boligrafo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_numero_17
+{
+    public class Boligrafo
+    {
+        const short cantidadTintaMaxima = 100;
+        ConsoleColor color;
+        short tinta;
+
+        public Boligrafo(short tinta,ConsoleColor color)
+        {
+            
+            this.tinta = tinta;
+            this.color = color;
+        }
+        public ConsoleColor GetColor() => color;
+        public short GetTinta() => tinta;
+
+        public bool Pintar(short gasto,out string dibujo)
+        {
+            bool bandera = false;
+            if (gasto >= 0 && gasto <= 100)
+            {
+                string asteriscos = "";
+                short tintaActual = GetTinta();
+                if(gasto > tintaActual)
+                {
+                    gasto = tintaActual;
+                }
+                for (int i = 0; i < gasto; i++)
+                {
+                    asteriscos += "*";
+                    tintaActual--;
+                }
+                this.SetTinta(tintaActual);
+                dibujo = asteriscos;
+
+                bandera = true;
+            }
+            else
+            {
+                dibujo = "";
+            }
+            return bandera;
+
+        }
+        public void SetTinta(short tinta)
+        {
+            if(tinta <= cantidadTintaMaxima && tinta >= 0)
+            {
+                this.tinta = tinta;
+            }
+        }
+        public void Recargar()
+        {
+            
+           this.SetTinta(cantidadTintaMaxima);
+        }
+    }
+}
---
+++ ejercicio numero 17/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_numero_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleColor color = ConsoleColor.Blue;
+            Boligrafo boligrafo = new Boligrafo(100,color);
+            bool flag = true;
+            short tintaUsada;
+            string asteriscos;
+            string opcion;
+            while(flag)
+            {
+                Console.Write("1. pintar\n2 recargar\n3 mostrar tinta\n4:salir:  ");
+                opcion = Console.ReadLine();
+                switch(opcion)
+                {
+                    case "1":
+                        Console.Write("Numero de tinta a usar: ");
+                        tintaUsada = Convert.ToInt16(Console.ReadLine());
+                        if(!boligrafo.Pintar(tintaUsada,out asteriscos))
+                        {
+                            Console.WriteLine("No se pudo pintar");
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = boligrafo.GetColor();
+                            Console.WriteLine("pinto {0}", asteriscos);
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                        }
+                        
+                        break;
+                    case "2":
+                        boligrafo.Recargar();
+                        break;
+                    case "3":
+                        Console.WriteLine("queda {0} de tinta", boligrafo.GetTinta());
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
